6d8847aa6ae6882f18d0d2cfc233935f714b6eb8
heenakashyap <heenakashyap005@gmail.com>
2020-09-30 13:47:06 -0700

    Parent Model Class Created

---
+++ src/main/java/com/csis3275/assignment_1_hka_05/models/BaseModel.java
@@ -0,0 +1,35 @@
+package com.csis3275.assignment_1_hka_05.models;
+
+public class BaseModel {
+	
+	protected String firstName;
+	protected String lastName;
+	protected String age;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getAge() {
+		return age;
+	}
+	
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+	
+
+}
