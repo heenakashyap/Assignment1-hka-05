2a12f37b67b5e411e637cfb7d6a11f7d658cdc0d
heenakashyap <heenakashyap005@gmail.com>
2020-09-30 14:05:29 -0700

    Added configuration files"

---
+++ src/main/java/com/csis3275/assignment_1_hka_05/config/ServletInitializer_hka_05.java
@@ -0,0 +1,24 @@
+package com.csis3275.assignment_1_hka_05.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@Configuration
+public class ServletInitializer_hka_05 implements WebApplicationInitializer{
+    public void onStartup(ServletContext ctx) throws ServletException {
+    	
+        AnnotationConfigWebApplicationContext webCtx = new AnnotationConfigWebApplicationContext();
+        webCtx.register(SpringMVCConfig_hka_05.class);
+        webCtx.setServletContext(ctx);
+        ServletRegistration.Dynamic servlet = ctx.addServlet("dispatcher", new DispatcherServlet(webCtx));
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+    
+    }
+}
---
+++ src/main/java/com/csis3275/assignment_1_hka_05/config/SpringMVCConfig_hka_05.java
@@ -0,0 +1,33 @@
+package com.csis3275.assignment_1_hka_05.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.csis3275.assignment_1_hka_05"})
+@Configuration
+public class SpringMVCConfig_hka_05 extends WebMvcConfigurerAdapter{
+   
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+    	configurer.enable();
+    }
+}
+
